639218c60ec5a39c6366ef9c04d71b3c95514b04
Andrew <g00398249@gmit.ie>
2021-01-29 15:38:03 +0000

    lab 2.2

---
+++ week02/hello2.py
@@ -0,0 +1,8 @@
+# Reads in a person's name then outputs a greeting for that person
+# Author: Andrew Scott
+
+# Asks the user to enter their name, which is then stored in a variable called name
+name = input("Enter your name: ")
+
+# Outputs a greeting using the name entered by the user
+print ("Hello " + name)
