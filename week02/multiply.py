2cc634f89d704ad41002dfc252c647bfa61b3ec7
Andrew <g00398249@gmit.ie>
2021-01-29 15:33:39 +0000

    lab 2.2

--- week02/multiply.py
+++ week02/multiply.py
@@ -1,5 +1,9 @@
 # This program prints the answer of 111 multiplied by 555
 # Author: Andrew Scott
 
+# The result of 111 multiplied by 55 is stored in a variable called answer
 answer = 111 * 555
+
+# Printing the variable answer is needed to provide the output that gives
+#the answer of 111 * 555
 print (answer)
