cec9b8d04d4facbd0d06c711df29a64ddaf9d2c4
Andrew <g00398249@gmit.ie>
2021-01-29 15:26:02 +0000

    lab 2.2

---
+++ week02/helloAndrew.py
@@ -0,0 +1,4 @@
+# Prints out the string Hello Andrew
+# Author: Andrew Scott
+
+print ("Hello Andrew")
---
+++ week02/multiply.py
@@ -0,0 +1,5 @@
+# This program prints the answer of 111 multiplied by 555
+# Author: Andrew Scott
+
+answer = 111 * 555
+print (answer)
