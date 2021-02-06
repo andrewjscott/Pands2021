8729a1065dc239a66faa2104f288216a216a413a
Andrew <g00398249@gmit.ie>
2021-02-06 15:31:11 +0000

    Week 03

---
+++ week03/3_1extra.py
@@ -0,0 +1,17 @@
+# My attempt to answer the extra questions in the lab 3.1
+# Andrew Scott
+
+# Added code as a comment so python wont run it and return an error
+# Below code won't work as it's trying to combine int to strings
+# message = 'I have eaten ' + 99 + ' burritos.'
+# print(message)
+
+# To fix it, add quotes around the int to make it a string
+
+message = 'I have eaten ' + "99" + ' burritos.'
+print(message)
+
+# eggs can be used as a variable name as it is not predefined by python, whereas 100 is predefined with the literal
+# numerical value of 100, so it makes no sense to try assign it a different value
+
+# The three functions are int(), float(), and str()
